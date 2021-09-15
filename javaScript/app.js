978c0ae00616ede5054ef4ca18dd46bc8ba16a78
Lívia Minucelli <liviawrk@gmail.com>
2021-09-15 01:31:12 -0300

    Projeto base terminado
    posso adicionar novos features antes de enviar

---
+++ javaScript/app.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //Animações ao iniciar a página index. (Pode ser aplicado a qualquer página da aplicação caso queira)
+    let tl1 = new TimelineMax();
+
+    tl1
+        .fromTo('.menu-center', 2,
+            { y: 50, opacity: 0, },
+            {
+                y: 0, opacity: 1, ease: Back.easeOut.config(1.7)
+            }, '-=0.5')
+
+        .fromTo('.menu-left', 0.7,
+            { y: 50, opacity: 0, },
+            {
+                y: 0, opacity: 1, ease: Back.easeOut.config(1.7)
+            }, '-=0.5')
+
+        .fromTo('.menu-right', 0.7,
+            { y: 50, opacity: 0, },
+            {
+                y: 0, opacity: 1, ease: Back.easeOut.config(1.7)
+            }, '-=0.5')
+        .fromTo('.container', 1.7,
+            { y: 50, opacity: 0, },
+            {
+                y: 0, opacity: 1, ease: Back.easeOut.config(1.7)
+            }, '-=0.5')
+        .fromTo('.footer-informations', 1.7,
+            { y: 50, opacity: 0, },
+            {
+                y: 0, opacity: 1, ease: Back.easeOut.config(1.7)
+            }, '-=0.5')
+})
--- javaScript/navbar.js
+++ javaScript/navbar.js
@@ -7,3 +7,5 @@ document.onreadystatechange = function () {
     document.getElementById('menu-btn').onclick = toggleMenu;
     document.getElementById('auxiliar-menu').onclick = toggleMenu;
 }
+
+//Código não utilizado, este código é um nav-menu para mobile. Achei interessante deixar a lógica utilizada aqui, mas n achei que servia para o projeto em si, decidi focar em mostrar que consigo trabalhar com responsividade no CSS e consigo me planejar utilizando as tags SEO. :)
