ceb34265f55dd056da36599a34364153895aee3b
Lívia Minucelli <liviawrk@gmail.com>
2021-09-15 15:19:17 -0300

    update

--- javaScript/navbar.js
+++ javaScript/navbar.js
@@ -8,4 +8,4 @@ document.onreadystatechange = function () {
     document.getElementById('auxiliar-menu').onclick = toggleMenu;
 }
 
-//Código não utilizado, este código é um nav-menu para mobile. Achei interessante deixar a lógica utilizada aqui, mas n achei que servia para o projeto em si, decidi focar em mostrar que consigo trabalhar com responsividade no CSS e consigo me planejar utilizando as tags SEO. :)
+//Código não utilizado, este código é um nav-menu para mobile. Achei interessante deixar a lógica utilizada aqui, porém não achei que servia para o projeto em si, decidi focar em mostrar que consigo trabalhar com responsividade no CSS, utilizando também o pré-processador SASS e como consigo me planejar utilizando as tags SEO e seus respectivos mecanismos. Ademais, animações com JS/CSS, W3C validation service para HTML e CSS :)
